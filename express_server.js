b020ab75765b8979bc280288d8c843d2e8650d2c
BugleJones <spencerjhamilton@gmail.com>
2017-09-12 21:53:44 +0000

    Added urls_new, added randomstring variable, updated index, partials

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  var newShortURL = "";
+  var possibleValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++) {
+    newShortURL += possibleValues.charAt(Math.floor(Math.random() * possibleValues.length));
+  }
+  return newShortURL;
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,8 +22,27 @@ var urlDatabase = {
 };
 
 app.get("/urls", (request, response) => {
-  let templateVars = {urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase
+  };
   response.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
+app.post("/urls", (request, response) => {
+  console.log(request.body);
+  response.send("Ok");
+});
+
+app.get("/urls/:id", (request, response) => {
+  let templateVars = {
+    shortURL: request.params.id,
+    keyURL: urlDatabase[request.params.id]
+  };
+  response.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (request, response) => {
